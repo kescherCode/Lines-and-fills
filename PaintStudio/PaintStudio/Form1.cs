373bdbe597e65c843b83072aaf4992d86ef3cdec
ErikWohl <45392986+ErikWohl@users.noreply.github.com>
2019-03-10 13:58:46 +0100

    Test

--- PaintStudio/PaintStudio/Form1.cs
+++ PaintStudio/PaintStudio/Form1.cs
@@ -299,5 +299,10 @@ namespace PaintStudio
             FillMethod = 0;
             ISDrawn = true;
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
