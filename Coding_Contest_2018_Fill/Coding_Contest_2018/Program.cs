1270d9bf8258e2c439007db9f6d1c5d07d951229
ErikWohl <45392986+ErikWohl@users.noreply.github.com>
2019-03-10 13:56:56 +0100

    Programms

---
+++ Coding_Contest_2018_Fill/Coding_Contest_2018/Program.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Coding_Contest_2018
+{
+    class Program
+    {
+        static int level = 2;
+        static int which = 1;
+        static int größe = 3;
+        static List<ConsoleColor> cl = new List<ConsoleColor>()
+        {
+            ConsoleColor.Blue, ConsoleColor.Cyan, ConsoleColor.DarkBlue, ConsoleColor.DarkCyan, ConsoleColor.DarkGray, ConsoleColor.DarkGreen,
+            ConsoleColor.DarkMagenta, ConsoleColor.DarkRed, ConsoleColor.DarkYellow, ConsoleColor.Green, ConsoleColor.Magenta,
+            ConsoleColor.Red, ConsoleColor.Yellow
+        };
+        static void Main(string[] args)
+        {
+            for (int f = 0; f < 2; f++)
+            {
+                which = f;
+                string filePathRead = $"level{level}_{f}.in";
+                string filePathWrite = $"level{level}_{f}_out.txt";
+                int col = 0;
+                int row = 0;
+                int anz = 0;
+                int[,] grid = new int[1, 1];
+                string[] split;
+                int r = 0, c = 0;
+                using (StreamReader sr = new StreamReader(filePathRead))
+                {
+                    string zeile = sr.ReadLine();
+                    if (anz == 0)
+                    {
+                        split = zeile.Split(' ');
+                        row = int.Parse(split[0]);
+                        col = int.Parse(split[1]);
+                        grid = new int[row, col];
+                    }
+                    while (!sr.EndOfStream)
+                    {
+                        zeile = sr.ReadLine();                        
+                        split = zeile.Split(' ');
+                        for (int i = 0; i < split.Length; i++)
+                        {
+                            if(split[i].Length < größe)
+                            {
+                                do
+                                {
+                                    split[i] += "0";
+                                } while (split[i].Length != größe);
+                            }
+                            grid[r, i] = int.Parse(split[i]);
+                        }
+                        Console.WriteLine(String.Join(" ", split));
+                        r++;
+                    }
+                    Console.ReadKey(true);
+                    ÜbergeordneterCrawler(ref grid);
+                    Dictionary<int, int> dict = new Dictionary<int, int>();
+                    for (int rw = 0; rw < grid.GetLength(0); rw++)
+                    {
+                        for (int cw = 0; cw < grid.GetLength(0); cw++)
+                        {
+                            if (grid[rw, cw] < 0)
+                            {
+                                if (!dict.ContainsKey(grid[rw, cw]))
+                                {
+                                    dict[grid[rw, cw]] = 0;
+                                }
+                                dict[grid[rw, cw]]++;
+                            }
+                        }
+                    }
+                    //List<int> counts = new List<int>();
+                    //foreach (var item in dict)
+                    //{
+                    //    counts.Add(item.Value);
+                    //}
+                    //counts.Sort();
+                    //using (StreamWriter sw = new StreamWriter(filePathWrite, false))
+                    //{
+                    //    string output = "";
+                    //    for (int i = 0; i < counts.Count; i++)
+                    //    {
+                    //        if (i != counts.Count - 1)
+                    //        {
+                    //            //Console.Write($"{i} {counts[i]} ");
+                    //            sw.Write($"{i} {counts[i]} ");
+                    //        }
+                    //        else
+                    //        {
+                    //            //Console.Write($"{i} {counts[i]}");
+                    //            sw.Write($"{i} {counts[i]}");
+                    //        }
+                    //    }
+                    //    //Console.WriteLine();
+                    //}
+                }
+                Console.Clear();
+            }
+            #region
+            /*
+            int aus = 0;
+            int house = 0;
+            bool gleiches = false;
+            List<int> h = new List<int>();
+            Dictionary<int, int[,]> höhen = new Dictionary<int, int[,]>();
+            int[,] test = new int[grid.GetLength(0), grid.GetLength(1)];
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int rw = 0; rw < grid.GetLength(0); rw++)
+            {
+                for (int cw = 0; cw < grid.GetLength(1); cw++)
+                {
+                    bool l = false;
+                    bool re = false;
+                    bool u = false;
+                    bool o = false;
+
+                    if (grid[rw, cw] > 0)
+                    {
+                        if (!höhen.Keys.Contains(grid[rw, cw]))
+                        {
+                            höhen[grid[rw, cw]] = new int[row, col];
+                            höhen[grid[rw, cw]][rw, cw] = grid[rw, cw];
+                        }
+                        int derh = grid[rw, cw];
+                        if (!h.Contains(derh))
+                            h.Add(derh);
+                        if (cw > 0)
+                        {
+                            if (derh == grid[rw, cw - 1])
+                            {
+                                l = true;
+                                höhen[derh][rw, cw - 1] = derh;
+                            }
+                        }
+                        if (cw < grid.GetLength(0) - 1)
+                        {
+                            if (derh == grid[rw, cw + 1])
+                            {
+                                re = true;
+                                höhen[derh][rw, cw + 1] = derh;
+                            }
+                        }
+                        if (rw > 0)
+                        {
+                            if (derh == grid[rw - 1, cw])
+                            {
+                                o = true;
+                                höhen[derh][rw - 1, cw] = derh;
+                            }
+                        }
+                        if (rw < grid.GetLength(1) - 1)
+                        {
+                            if (derh == grid[rw + 1, cw])
+                            {
+                                u = true;
+                                höhen[derh][rw + 1, cw] = derh;
+                            }
+                        }
+                        if (u == false && o == false && re == false && l == false)
+                        {
+                            for (int kw = 0; kw < höhen[derh].GetLength(0); kw++)
+                            {
+                                for (int lw = 0; lw < höhen[derh].GetLength(0); lw++)
+                                {
+                                    if (höhen[derh][kw, lw] != 0)
+                                    {
+                                        if (!dict.Keys.Contains(aus))
+                                            dict[aus] = 0;
+                                        dict[aus]++;
+                                    }
+                                }
+                            }
+                            if (höhen.Keys.Contains(grid[rw, cw]))
+                            {
+                                höhen.Remove(grid[rw, cw]);
+                            }
+                        }
+                    }
+                }
+                if (house == 1)
+                    break;
+                aus = 0;
+
+            }
+            for (int ha = 0; ha < h.Count; ha++)
+            {
+                for (int kw = 0; kw < höhen[h[ha]].GetLength(0); kw++)
+                {
+                    for (int lw = 0; lw < höhen[h[ha]].GetLength(0); lw++)
+                    {
+                        if (höhen[h[ha]][kw, lw] == h[ha])
+                        {
+                            if (!dict.Keys.Contains(h[ha]))
+                                dict[h[ha]] = 0;
+                            dict[h[ha]]++;
+                        }
+                    }
+                }
+                if (h[ha] == 58)
+                    ÜbergeordneterCrawler(höhen[h[ha]]);
+            }
+            */
+            #endregion
+            ConsoleKey k = Console.ReadKey(true).Key;
+            do
+            {
+                k = Console.ReadKey(true).Key;
+            } while (k != ConsoleKey.Escape);
+        }
+
+        static int neg = -int.Parse(100000000000.ToString().Substring(0, größe - 1));
+        static int zahl = 10;
+        public static void ÜbergeordneterCrawler(ref int[,] grid)
+        {
+            for (int r = 0; r < grid.GetLength(0); r++)
+            {
+                for (int c = 0; c < grid.GetLength(1); c++)
+                {
+                    if (grid[r, c] > 0)
+                    {
+                        zahl = grid[r, c];
+                        Suche(ref grid, r, c);
+                        Console.ReadKey(true);
+                        neg--;
+                    }
+                }
+            }
+            //GridAufteilen(grid);
+        }
+
+        public static void Suche(ref int[,] grid, int row, int col)
+        {
+            if (row < 0 || row > grid.GetLength(0) - 1)
+                return;
+            if (col < 0 || col > grid.GetLength(0) - 1)
+                return;
+            if (grid[row, col] == zahl)
+            {
+                grid[row, col] = neg;
+                Console.SetCursorPosition(col * (grid[row, col].ToString().Length + 1), row);
+                Console.ForegroundColor = cl[Math.Abs(neg) % cl.Count];
+                Console.Write(neg);
+                System.Threading.Thread.Sleep(10);
+                Console.ForegroundColor = ConsoleColor.White;
+                Suche(ref grid, row, col - 1);
+                Suche(ref grid, row, col + 1);
+                Suche(ref grid, row - 1, col);
+                Suche(ref grid, row + 1, col);
+            }
+        }
+
+        public static void GridAufteilen(int[,] grid)
+        {
+            using (StreamWriter sw = new StreamWriter($"Ausgabe_{level}_{which}.txt", false))
+            {
+                string output = "";
+                for (int r = 0; r < grid.GetLength(0); r++)
+                {
+                    for (int c = 0; c < grid.GetLength(0); c++)
+                    {
+                        if (grid[r, c] == 0)
+                            sw.Write($"000 ");
+                        else
+                            sw.Write($"{grid[r, c]} ");
+
+                    }
+                    sw.WriteLine();
+                }
+
+            }
+
+        }
+    }
+}
+
---
+++ FloodFill_Scanfill/FloodFill_Scanfill/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FloodFill_Scanfill
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void FloodFill(Bitmap bmp, Point pt, Color targetColor, Color replacementColor)
+        {
+            Stack<Point> pixels = new Stack<Point>();
+            targetColor = bmp.GetPixel(pt.X, pt.Y);
+            pixels.Push(pt);
+
+            while (pixels.Count > 0)
+            {
+                Point a = pixels.Pop();
+                if (a.X < bmp.Width && a.X > 0 &&
+                        a.Y < bmp.Height && a.Y > 0)//make sure we stay within bounds
+                {
+                    if (bmp.GetPixel(a.X, a.Y) == targetColor)
+                    {
+                        bmp.SetPixel(a.X, a.Y, replacementColor);
+                        pixels.Push(new Point(a.X - 1, a.Y));
+                        pixels.Push(new Point(a.X + 1, a.Y));
+                        pixels.Push(new Point(a.X, a.Y - 1));
+                        pixels.Push(new Point(a.X, a.Y + 1));
+                        pictureBox1.Refresh();
+                    }
+                }
+            }
+            pictureBox1.Refresh(); //refresh our main picture box
+            return;
+        }
+        private void ScanFill(Bitmap bmp, Point pt, Color targetColor, Color replacementColor)
+        {
+            targetColor = bmp.GetPixel(pt.X, pt.Y);
+            if (targetColor.ToArgb().Equals(replacementColor.ToArgb()))
+            {
+                replacementColor = Color.Crimson;
+            }
+
+            Stack<Point> pixels = new Stack<Point>();
+
+            pixels.Push(pt);
+            while (pixels.Count != 0)
+            {
+                Point temp = pixels.Pop();
+                int y1 = temp.Y;
+                while (y1 >= 0 && bmp.GetPixel(temp.X, y1) == targetColor)
+                {
+                    y1--;
+                }
+                y1++;
+                bool spanLeft = false;
+                bool spanRight = false;
+                while (y1 < bmp.Height && bmp.GetPixel(temp.X, y1) == targetColor)
+                {
+                    bmp.SetPixel(temp.X, y1, replacementColor);
+                    pictureBox1.Refresh();
+
+                    if (!spanLeft && temp.X > 0 && bmp.GetPixel(temp.X - 1, y1) == targetColor)
+                    {
+                        pixels.Push(new Point(temp.X - 1, y1));
+                        spanLeft = true;
+                    }
+                    else if (spanLeft && temp.X - 1 == 0 && bmp.GetPixel(temp.X - 1, y1) != targetColor)
+                    {
+                        spanLeft = false;
+                    }
+                    if (!spanRight && temp.X < bmp.Width - 1 && bmp.GetPixel(temp.X + 1, y1) == targetColor)
+                    {
+                        pixels.Push(new Point(temp.X + 1, y1));
+                        spanRight = true;
+                    }
+                    else if (spanRight && temp.X < bmp.Width - 1 && bmp.GetPixel(temp.X + 1, y1) != targetColor)
+                    {
+                        spanRight = false;
+                    }
+                    y1++;
+                }
+
+            }
+            pictureBox1.Refresh();
+
+        }
+        private void FloodFillQueue(Bitmap bmp, Point pt, Color targetColor, Color replacementColor)
+        {
+            Queue<Point> pixels = new Queue<Point>();
+            targetColor = bmp.GetPixel(pt.X, pt.Y);
+            pixels.Enqueue(pt);
+
+            while (pixels.Count > 0)
+            {
+                Point a = pixels.Dequeue();
+                if (a.X < bmp.Width && a.X > 0 &&
+                        a.Y < bmp.Height && a.Y > 0)//make sure we stay within bounds
+                {
+                    if (bmp.GetPixel(a.X, a.Y) == targetColor)
+                    {
+                        bmp.SetPixel(a.X, a.Y, replacementColor);
+                        pixels.Enqueue(new Point(a.X - 1, a.Y));
+                        pixels.Enqueue(new Point(a.X + 1, a.Y));
+                        pixels.Enqueue(new Point(a.X, a.Y - 1));
+                        pixels.Enqueue(new Point(a.X, a.Y + 1));
+                        pictureBox1.Refresh();
+                    }
+                }
+            }
+            pictureBox1.Refresh(); //refresh our main picture box
+            return;
+        }
+        private void ScanFillQueue(Bitmap bmp, Point pt, Color targetColor, Color replacementColor)
+        {
+            targetColor = bmp.GetPixel(pt.X, pt.Y);
+            if (targetColor.ToArgb().Equals(replacementColor.ToArgb()))
+            {
+                replacementColor = Color.Crimson;
+            }
+
+            Queue<Point> pixels = new Queue<Point>();
+
+            pixels.Enqueue(pt);
+            while (pixels.Count != 0)
+            {
+                Point temp = pixels.Dequeue();
+                int y1 = temp.Y;
+                while (y1 >= 0 && bmp.GetPixel(temp.X, y1) == targetColor)
+                {
+                    y1--;
+                }
+                y1++;
+                bool spanLeft = false;
+                bool spanRight = false;
+                while (y1 < bmp.Height && bmp.GetPixel(temp.X, y1) == targetColor)
+                {
+                    bmp.SetPixel(temp.X, y1, replacementColor);
+                    pictureBox1.Refresh();
+
+                    if (!spanLeft && temp.X > 0 && bmp.GetPixel(temp.X - 1, y1) == targetColor)
+                    {
+                        pixels.Enqueue(new Point(temp.X - 1, y1));
+                        spanLeft = true;
+                    }
+                    else if (spanLeft && temp.X - 1 == 0 && bmp.GetPixel(temp.X - 1, y1) != targetColor)
+                    {
+                        spanLeft = false;
+                    }
+                    if (!spanRight && temp.X < bmp.Width - 1 && bmp.GetPixel(temp.X + 1, y1) == targetColor)
+                    {
+                        pixels.Enqueue(new Point(temp.X + 1, y1));
+                        spanRight = true;
+                    }
+                    else if (spanRight && temp.X < bmp.Width - 1 && bmp.GetPixel(temp.X + 1, y1) != targetColor)
+                    {
+                        spanRight = false;
+                    }
+                    y1++;
+                }
+
+            }
+            pictureBox1.Refresh();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FloodFill((Bitmap)pictureBox1.Image, new Point(100, 10), Color.White, Color.Yellow);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ScanFill((Bitmap)pictureBox1.Image, new Point(100, 10), Color.White, Color.Blue);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FloodFillQueue((Bitmap)pictureBox1.Image, new Point(100, 100), Color.White, Color.Yellow);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ScanFillQueue((Bitmap)pictureBox1.Image, new Point(100, 10), Color.White, Color.Blue);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Title = "Please select the image";
+            fd.InitialDirectory = Directory.GetCurrentDirectory();
+            fd.ShowDialog();
+            string filename = fd.FileName;
+            pictureBox1.Image = new Bitmap(filename);
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+            Point p = e.Location;
+        }
+    }
+}
