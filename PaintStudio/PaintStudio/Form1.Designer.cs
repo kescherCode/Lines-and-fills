3f821a3fa54544ffe4f1def0dbf04546e39dd20f
ErikWohl <45392986+ErikWohl@users.noreply.github.com>
2019-03-10 13:14:15 +0100

    Paint Programm

---
+++ PaintStudio/PaintStudio/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace PaintStudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button6 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PaintStudio.Properties.Resources.UnfilledCanvas;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1800, 900);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1807, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "FloodFill";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1807, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 59);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "FloodFillQ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1807, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 59);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "ScanFill";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(1807, 207);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 59);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "ScanFillQ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(1807, 272);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 59);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "RESET";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.Color = System.Drawing.Color.DarkRed;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(1807, 337);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(105, 59);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Color";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(1807, 402);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(105, 59);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "DrawLine";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ PaintStudio/PaintStudio/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintStudio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static bool ISDrawn = false;
+        static Point p1 = new Point();
+        static Point p2 = new Point();
+        static int FillMethod = 0;
+        static Color c;
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch(FillMethod)
+            {
+                case 1:
+                    FloodFill((Bitmap)pictureBox1.Image, e.Location, ((Bitmap)pictureBox1.Image).GetPixel(e.X, e.Y), c);
+                    break;
+                case 2:
+                    FloodFillQueue((Bitmap)pictureBox1.Image, e.Location, ((Bitmap)pictureBox1.Image).GetPixel(e.X, e.Y), c);
+                    break;
+                case 3:
+                    ScanFill((Bitmap)pictureBox1.Image, e.Location, ((Bitmap)pictureBox1.Image).GetPixel(e.X, e.Y), c);
+                    break;
+                case 4:
+                    ScanFillQueue((Bitmap)pictureBox1.Image, e.Location, ((Bitmap)pictureBox1.Image).GetPixel(e.X, e.Y), c);
+                    break;
+                default:
+                    if(ISDrawn)
+                    {
+                        if (p1.IsEmpty)
+                            p1 = e.Location;
+                        else                           
+                        {
+                            p2 = e.Location;
+                            DrawLine((Bitmap)pictureBox1.Image, p1, p2, c);
+                            pictureBox1.Refresh();
+                            p1 = new Point();
+                            p2 = new Point();
+                        }
+                    }
+                    break;
+            }
+        }
+        static bool down = false;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            down = !down;
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+        }
+        #region FillMethoden
+        private void FloodFill(Bitmap bmp, Point pt, Color targetColor, Color replacementColor)
+        {
+            Stack<Point> pixels = new Stack<Point>();
+            targetColor = bmp.GetPixel(pt.X, pt.Y);
+            pixels.Push(pt);
+
+            while (pixels.Count > 0)
+            {
+                Point a = pixels.Pop();
+                if (a.X < bmp.Width && a.X > 0 &&
+                        a.Y < bmp.Height && a.Y > 0)//make sure we stay within bounds
+                {
+                    if (bmp.GetPixel(a.X, a.Y) == targetColor)
+                    {
+                        bmp.SetPixel(a.X, a.Y, replacementColor);
+                        pixels.Push(new Point(a.X - 1, a.Y));
+                        pixels.Push(new Point(a.X + 1, a.Y));
+                        pixels.Push(new Point(a.X, a.Y - 1));
+                        pixels.Push(new Point(a.X, a.Y + 1));
+                        //pictureBox1.Refresh();
+                    }
+                }
+            }
+            pictureBox1.Refresh(); //refresh our main picture box
+            return;
+        }
+        private void ScanFill(Bitmap bmp, Point pt, Color targetColor, Color replacementColor)
+        {
+            targetColor = bmp.GetPixel(pt.X, pt.Y);
+            if (targetColor.ToArgb().Equals(replacementColor.ToArgb()))
+            {
+                replacementColor = Color.Crimson;
+            }
+
+            Stack<Point> pixels = new Stack<Point>();
+
+            pixels.Push(pt);
+            while (pixels.Count != 0)
+            {
+                Point temp = pixels.Pop();
+                int y1 = temp.Y;
+                while (y1 >= 0 && bmp.GetPixel(temp.X, y1) == targetColor)
+                {
+                    y1--;
+                }
+                y1++;
+                bool spanLeft = false;
+                bool spanRight = false;
+                while (y1 < bmp.Height && bmp.GetPixel(temp.X, y1) == targetColor)
+                {
+                    bmp.SetPixel(temp.X, y1, replacementColor);
+                    //pictureBox1.Refresh();
+
+                    if (!spanLeft && temp.X > 0 && bmp.GetPixel(temp.X - 1, y1) == targetColor)
+                    {
+                        pixels.Push(new Point(temp.X - 1, y1));
+                        spanLeft = true;
+                    }
+                    else if (spanLeft && temp.X - 1 == 0 && bmp.GetPixel(temp.X - 1, y1) != targetColor)
+                    {
+                        spanLeft = false;
+                    }
+                    if (!spanRight && temp.X < bmp.Width - 1 && bmp.GetPixel(temp.X + 1, y1) == targetColor)
+                    {
+                        pixels.Push(new Point(temp.X + 1, y1));
+                        spanRight = true;
+                    }
+                    else if (spanRight && temp.X < bmp.Width - 1 && bmp.GetPixel(temp.X + 1, y1) != targetColor)
+                    {
+                        spanRight = false;
+                    }
+                    y1++;
+                }
+
+            }
+            pictureBox1.Refresh();
+
+        }
+        private void FloodFillQueue(Bitmap bmp, Point pt, Color targetColor, Color replacementColor)
+        {
+            Queue<Point> pixels = new Queue<Point>();
+            targetColor = bmp.GetPixel(pt.X, pt.Y);
+            pixels.Enqueue(pt);
+
+            while (pixels.Count > 0)
+            {
+                Point a = pixels.Dequeue();
+                if (a.X < bmp.Width && a.X > 0 &&
+                        a.Y < bmp.Height && a.Y > 0)//make sure we stay within bounds
+                {
+                    if (bmp.GetPixel(a.X, a.Y) == targetColor)
+                    {
+                        bmp.SetPixel(a.X, a.Y, replacementColor);
+                        pixels.Enqueue(new Point(a.X - 1, a.Y));
+                        pixels.Enqueue(new Point(a.X + 1, a.Y));
+                        pixels.Enqueue(new Point(a.X, a.Y - 1));
+                        pixels.Enqueue(new Point(a.X, a.Y + 1));
+                        //pictureBox1.Refresh();
+                    }
+                }
+            }
+            pictureBox1.Refresh(); //refresh our main picture box
+            return;
+        }
+        private void ScanFillQueue(Bitmap bmp, Point pt, Color targetColor, Color replacementColor)
+        {
+            targetColor = bmp.GetPixel(pt.X, pt.Y);
+            if (targetColor.ToArgb().Equals(replacementColor.ToArgb()))
+            {
+                replacementColor = Color.Crimson;
+            }
+
+            Queue<Point> pixels = new Queue<Point>();
+
+            pixels.Enqueue(pt);
+            while (pixels.Count != 0)
+            {
+                Point temp = pixels.Dequeue();
+                int y1 = temp.Y;
+                while (y1 >= 0 && bmp.GetPixel(temp.X, y1) == targetColor)
+                {
+                    y1--;
+                }
+                y1++;
+                bool spanLeft = false;
+                bool spanRight = false;
+                while (y1 < bmp.Height && bmp.GetPixel(temp.X, y1) == targetColor)
+                {
+                    bmp.SetPixel(temp.X, y1, replacementColor);
+                    //pictureBox1.Refresh();
+
+                    if (!spanLeft && temp.X > 0 && bmp.GetPixel(temp.X - 1, y1) == targetColor)
+                    {
+                        pixels.Enqueue(new Point(temp.X - 1, y1));
+                        spanLeft = true;
+                    }
+                    else if (spanLeft && temp.X - 1 == 0 && bmp.GetPixel(temp.X - 1, y1) != targetColor)
+                    {
+                        spanLeft = false;
+                    }
+                    if (!spanRight && temp.X < bmp.Width - 1 && bmp.GetPixel(temp.X + 1, y1) == targetColor)
+                    {
+                        pixels.Enqueue(new Point(temp.X + 1, y1));
+                        spanRight = true;
+                    }
+                    else if (spanRight && temp.X < bmp.Width - 1 && bmp.GetPixel(temp.X + 1, y1) != targetColor)
+                    {
+                        spanRight = false;
+                    }
+                    y1++;
+                }
+
+            }
+            pictureBox1.Refresh();
+
+        }
+        #endregion
+
+        private Bitmap DrawLine(Bitmap bitmap, Point p1, Point p2, Color c)
+        {
+            int dx = Math.Abs(p2.X - p1.X);
+            int sx = p1.X < p2.X ? 1 : -1;
+            int dy = Math.Abs(p2.Y - p1.Y);
+            int sy = p1.Y < p2.Y ? 1 : -1;
+            int err = (dx > dy ? dx : -dy) / 2;
+            int e2;
+            for (; ; )
+            {
+                bitmap.SetPixel(p1.X, p1.Y, c);
+                if (p1 == p2)
+                    break;
+                e2 = err;
+                if (e2 > -dx)
+                {
+                    err -= dy;
+                    p1.X += sx;
+                }
+                if (e2 < dy)
+                {
+                    err += dx;
+                    p1.Y += sy;
+                }
+            }
+            return bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FillMethod = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FillMethod = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FillMethod = 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FillMethod = 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FillMethod = 0;
+            ISDrawn = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            c = colorDialog1.Color;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Graphics g = Graphics.FromImage(pictureBox1.Image);
+            //g.DrawLine
+            if (down == true)
+            {
+                ((Bitmap)pictureBox1.Image).SetPixel(e.X, e.Y, Color.Black);
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FillMethod = 0;
+            ISDrawn = true;
+        }
+    }
+}
